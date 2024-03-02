2620c94adf9839a77b351d71b8f96df90ebe2f02
Oumaima Saadi <73801542+ouma-eriroha@users.noreply.github.com>
2024-03-02 02:03:34 +0100

    Update

--- src/main/java/tn/esprit/controllers/SportifProfileController.java
+++ src/main/java/tn/esprit/controllers/SportifProfileController.java
@@ -1,4 +1,33 @@
 package tn.esprit.controllers;
 
-public class SportifProfileController {
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class SportifProfileController implements Initializable {
+
+    @FXML
+    private Button ReportsBtn;
+
+    @FXML
+    private Button UsersBtn;
+
+    @FXML
+    private Button ProductManagBtn;
+
+    @FXML
+    private Button logoutBtn;
+
+    @FXML
+    private Button HomeBtn;
+
+    @FXML
+    private Button GrowthBtn;
+    @FXML
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {}
+
 }
